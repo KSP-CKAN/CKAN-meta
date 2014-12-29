fdefa054e336adde88fd3484da8e156c90a15e94
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-30 00:29:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo Commit hash: ${ghprbActualCommit}
 echo Changes in this commit:
-export COMMIT_CHANGES=`git diff ${ghprbActualCommit}^1 ${ghprbActualCommit}`
+export COMMIT_CHANGES=`git diff-tree --name-only `git rev-parse HEAD`..${ghprbActualCommit}`
 echo ${COMMIT_CHANGES}
 
 wget https://raw.githubusercontent.com/KSP-CKAN/CKAN/master/bin/ckan-validate.py -O ckan-validate.py
